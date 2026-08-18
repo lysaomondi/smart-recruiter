563cd99464e431ac69555e6d29b92724853e2bf1
lysa omondi <lisabethkidangasi@gmail.com>
2026-08-18 10:07:10 +0300

    feat: add edit-assessment route

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayout from "../components/layout/DashboardLayout";
 import RecruiterDashboard from "../pages/recruiter/RecruiterDashboard";
 import CreateAssessment from "../pages/recruiter/CreateAssessment";
+import EditAssessment from "../pages/recruiter/EditAssessment";
 
 export default function AppRoutes() {
   const handleLogout = () => {
@@ -14,6 +15,7 @@ export default function AppRoutes() {
       <Route element={<DashboardLayout role="recruiter" onLogout={handleLogout} />}>
         <Route path="/recruiter/dashboard" element={<RecruiterDashboard />} />
         <Route path="/recruiter/assessments/create" element={<CreateAssessment />} />
+        <Route path="/recruiter/assessments/:id/edit" element={<EditAssessment />} />
       </Route>
       <Route path="/" element={<Navigate to="/recruiter/dashboard" replace />} />
     </Routes>
