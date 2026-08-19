85716005fb56669471e9a6e7636be790f1148ad1
janenyasoro <janenyasoro@gmail.com>
2026-08-19 08:35:28 +0300

    feat: add interviewee routes for assessment flow

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ProtectedRoute from '../components/common/ProtectedRoute';
+import Login from '../pages/auth/Login';
+import Register from '../pages/auth/Register';
+import IntervieweeDashboard from '../pages/interviewee/IntervieweeDashboard';
+import AssessmentInstructions from '../pages/interviewee/AssessmentInstructions';
+import TakeAssessment from '../pages/interviewee/TakeAssessment';
+import TrialAssessment from '../pages/interviewee/TrialAssessment';
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* Interviewee Routes */}
+            <Route
+                path="/interviewee/dashboard"
+                element={
+                    <ProtectedRoute role="interviewee">
+                        <IntervieweeDashboard />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/interviewee/assessment/:assessmentId/instructions"
+                element={
+                    <ProtectedRoute role="interviewee">
+                        <AssessmentInstructions />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/interviewee/assessment/:assessmentId/take"
+                element={
+                    <ProtectedRoute role="interviewee">
+                        <TakeAssessment />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/interviewee/trial"
+                element={
+                    <ProtectedRoute role="interviewee">
+                        <TrialAssessment />
+                    </ProtectedRoute>
+                }
+            />
+
+            {/* Default redirect */}
+            <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
